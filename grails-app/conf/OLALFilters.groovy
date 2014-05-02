8f9a15c41d4a296f8cb502bb8b977872509f8e4b
Anton Mostovoy <amostovoy@orbitz.com>
2014-05-02 15:38:06 -0500

    initial commit (0.2)

---
+++ grails-app/conf/OLALFilters.groovy
@@ -0,0 +1,89 @@
+import grails.util.GrailsNameUtils
+
+import javax.annotation.PostConstruct
+import java.util.regex.Pattern
+
+class OLALFilters {
+
+    private static final String START_TIME_ATTRIBUTE = 'OLALController__START_TIME__'
+    private static final String VIEW_START_TIME_ATTRIBUTE = 'OLALController__VIEW_START_TIME__'
+
+    def grailsApplication
+    def dependsOn = []
+
+    private Pattern filterParamRegExp
+
+    @PostConstruct
+    def init() {
+        def conf = grailsApplication.config.grails.plugins.olal.dependsOnFilters
+        dependsOn =   conf ? conf : []
+
+        def paramFilterConf = grailsApplication.config.grails.plugins.olal.addlParamsToFilter
+
+        if (paramFilterConf) {
+            def patterns = paramFilterConf.clone()
+            paramFilterConf.each {
+                patterns << ".*\\.$it"
+            }
+            println patterns
+            filterParamRegExp = Pattern.compile(patterns.join('|'), Pattern.CASE_INSENSITIVE)
+        }
+    }
+
+    def filters = {
+        all(controller: '*', action: '*') {
+            before = {
+                request[START_TIME_ATTRIBUTE] = System.currentTimeMillis()
+            }
+
+            after = { Map model ->
+                request[VIEW_START_TIME_ATTRIBUTE] = System.currentTimeMillis()
+            }
+            afterView = { Exception e ->
+                long now = System.currentTimeMillis()
+                long total = now - request[START_TIME_ATTRIBUTE]
+                long view = now - request[VIEW_START_TIME_ATTRIBUTE]
+                def formattedCtrlName = formatControllerName(controllerName)
+                def actionParamsOnly = getActionParams(params)
+
+                int actionPct = (double)(view * 100) / total
+                log.info("($request.remoteAddr) Status $response.status in ${total/1000}s (view $actionPct%) for [$request.method] to $formattedCtrlName#$actionName ${actionParamsOnly} ")
+
+            }
+        }
+    }
+
+    def formatControllerName(name) {
+        GrailsNameUtils.getClassName(name, 'Controller')
+    }
+
+    def getActionParams(params) {
+        Map queryParams = [:] << params
+        queryParams.remove('action')
+        queryParams.remove('controller')
+        queryParams.remove('format')
+
+        if(filterParamRegExp) {
+            return filterParams(queryParams)
+        } else {
+            return queryParams;
+        }
+    }
+    def filterParams(Map params) {
+
+        def filtered = [:]
+
+        params.each { String key, value ->
+            if (filterParamRegExp.matcher(key).matches()) {
+                value = '[FILTERED]'
+            } else if (value instanceof Map) {
+                value = filterParams(value)
+            } else if (value.class.isArray()) {
+                value.each { it instanceof Map ? filterParams(it) : it }
+            }
+
+            filtered[key] = value
+        }
+        return filtered
+    }
+}
---
+++ test/unit/filters/TrackingFilterFiltersSpec.groovy
@@ -0,0 +1,17 @@
+package filters
+
+import grails.test.mixin.Mock
+import spock.lang.Specification
+
+@Mock(TrackingFilterFilters)
+class TrackingFilterFiltersSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
