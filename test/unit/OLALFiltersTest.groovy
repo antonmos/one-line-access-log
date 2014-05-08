421bd5c55ee614f4d9ca92b3efd696b2d6fee761
Anton Mostovoy <amostovoy@orbitz.com>
2014-05-08 00:14:30 -0500

    add more tests

--- test/unit/OLALFiltersTest.groovy
+++ test/unit/OLALFiltersTest.groovy
@@ -1,7 +1,41 @@
+import filters.OLALFilters
+import filters.ParameterFilter
+import grails.test.mixin.TestFor
+import org.apache.commons.logging.Log
 import spock.lang.Specification
 
-/**
- * Created by amostovoy on 5/7/14.
- */
+@TestFor(OLALFilters)
 class OLALFiltersTest extends Specification {
+
+    def setupSpec() {
+        grailsApplication.config.grails.exceptionresolver.params.exclude = ['password']
+    }
+    def testFilter() {
+
+        setup:
+        def mockLogger = Mock(Log)
+        OLALFilters filter = grailsApplication.mainContext.getBean('filters.OLALFilters')
+        filter.log = mockLogger
+        when:
+        //Grails adds these to the params has that the filters get, but withFilters doesnt do it.
+        params['action'] = 'index'
+        params['controller'] = 'test'
+        params['format'] = 'test'
+        params['password'] = 'secret'
+
+        withFilters ( controller: 'test', action: 'index' ) {
+        }
+
+        then:
+           response.status == 200
+           1 * mockLogger.info({
+               def statusOk = it.contains("Status 200")
+               def params = it[it.lastIndexOf('#index')+7..it.lastIndexOf(']')]
+               def paramsOk = !params.contains('index') && !params.contains('controller') && !params.contains('format')
+               println params
+               def filteringOk = params.contains(ParameterFilter.REPLACEMENT)
+               def controllerAndActionOk = it.contains("TestController#index")
+               statusOk && paramsOk && controllerAndActionOk && filteringOk
+           })
+    }
 }
